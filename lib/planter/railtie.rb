f03e5fc491d573c5f100ebc7987c551de0523de5
Ben Lim <blim8183@gmail.com>
2016-09-12 16:01:40 -0400

    fix reference to ablecop

--- lib/planter/railtie.rb
+++ lib/planter/railtie.rb
@@ -8,7 +8,7 @@ module Planter
 
     # Public: Expose our rake tasks to the Rails application.
     rake_tasks do
-      load "ablecop/tasks/planter.rake"
+      load "planter/tasks/planter.rake"
     end
   end
 end
