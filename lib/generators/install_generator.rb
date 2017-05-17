f123d5d2fcb4b2fc409f897945ef7a7934dc5b78
Ben Lim <blim8183@gmail.com>
2017-05-17 16:31:29 -0400

    fix generator

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,15 +1,27 @@
 require "rails/generators"
 
-module Planter
-  # Public: `rails generate planter:install`
-  class InstallGenerator < Rails::Generators::Base
-    desc "Create the default.rb seed file and put it in db/plant."
+# Public: `rails generate planter:install`
+class InstallGenerator < Rails::Generators::Base
+  desc "Create the default.rb seed file and put it in db/plant."
 
-    source_root File.expand_path("../../../../templates", __FILE__)
+  source_root File.expand_path("../../../../templates", __FILE__)
 
-    def create_default_seed_file
-      copy_file "db/plant/default.rb", "db/plant/default.rb"
-      copy_file "config/initializers/planter.rb", "config/initializers/planter.rb"
-    end
+  def create_default_seed_file
+    copy_file "db/plant/default.rb", "db/plant/default.rb"
+    copy_file "config/initializers/planter.rb", "config/initializers/planter.rb"
   end
 end
+
+# module Planter
+#   # Public: `rails generate planter:install`
+#   class InstallGenerator < Rails::Generators::Base
+#     desc "Create the default.rb seed file and put it in db/plant."
+#
+#     source_root File.expand_path("../../../../templates", __FILE__)
+#
+#     def create_default_seed_file
+#       copy_file "db/plant/default.rb", "db/plant/default.rb"
+#       copy_file "config/initializers/planter.rb", "config/initializers/planter.rb"
+#     end
+#   end
+# end
