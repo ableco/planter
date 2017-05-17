456a45fcfbaa7f005f44ede888dbe86ba7145b84
Ben Lim <blim8183@gmail.com>
2017-05-17 16:23:20 -0400

    require the necessary files and add the configure method

--- lib/planter.rb
+++ lib/planter.rb
@@ -1,7 +1,16 @@
 require "planter/engine"
+require "planter/base"
+require "planter/github_issue_parser"
+require "planter/bootstrapper"
+require "octokit"
 
 module Planter
   class << self
     attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
   end
 end
