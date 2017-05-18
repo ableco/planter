2287134de62a4772f25e99386856984e19a81969
Ben Lim <blim8183@gmail.com>
2017-05-17 22:09:54 -0400

    Removing seeds controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Planter::Engine.routes.draw do
-  resources :seeds, format: "html"
-  resources :plants, format: "html"
+  root "plants#index"
+  resources :plants, only: [:index] do
+    put :reset
+  end
 end
