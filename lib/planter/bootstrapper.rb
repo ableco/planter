377fe22cfdde77de3c9a2a00093f749b1711beda
Benjamin Lim <blim8183@gmail.com>
2017-05-23 12:02:40 -0400

    QaEnvironment class and Bootstrapper refactor (#12)
    * Create a QaEnvironment class
    
    * Rewriting Planter::Bootstrapper a little bit so that it will be easier
    to run an individual plant file.
    
    * Specify default `seeding` values for the run and run_all methods.

--- lib/planter/bootstrapper.rb
+++ lib/planter/bootstrapper.rb
@@ -4,29 +4,35 @@ module Planter
   # Bootstrapping qa data.
   class Bootstrapper
     class << self
+      # Run the `seed` method for all of the associated plant files.
       def seed
-        @seeding = true
-        run
+        run_all(seeding: true)
       end
 
+      # Run the `deseed` method for all of the associated plant files.
       # "Deseed" is a real word: http://www.merriam-webster.com/dictionary/deseed
       def deseed
-        @seeding = false
-        run
+        run_all(seeding: false)
       end
 
-      private
+      # Accepts an issue number and boolean and runs the `seed` or `deseed`
+      # method of the plant file corresponding to the issue number.
+      def run(issue_number:, seeding: true)
+        if defined?(Plant::Default) || load_default_seed_file
+          default_seed = Plant::Default.new(issue_number)
+          seeding ? default_seed.seed : default_seed.deseed
+        end
 
-      def run
-        issue_numbers.each do |issue_number|
-          if defined?(Plant::Default) || load_default_seed_file
-            default_seed = Plant::Default.new(issue_number)
-            seeding? ? default_seed.seed : default_seed.deseed
-          end
-
-          next unless load_issue_specific_seed_file(issue_number)
+        if load_issue_specific_seed_file(issue_number)
           issue_seed = Plant.const_get("Issue#{issue_number}").new(issue_number)
-          seeding? ? issue_seed.seed : issue_seed.deseed
+          seeding ? issue_seed.seed : issue_seed.deseed
+        end
+      end
+
+      # Calls `run` for each issue number associated with the QaEnvironment.
+      def run_all(seeding: true)
+        QaEnvironment.issue_numbers.each do |issue_number|
+          run(issue_number: issue_number, seeding: seeding)
         end
       end
 
@@ -40,42 +46,6 @@ module Planter
         require "#{Rails.root}/db/plant/issue_#{issue_number}.rb"
       rescue LoadError
         false
-      end
-
-      def seeding?
-        @seeding
-      end
-
-      # Parse the pull request's body for the numbers of the issues being QA'd.
-      def issue_numbers
-        pull_request = pull_request(repository_full_name, pull_request_number)
-        Planter::GithubIssueParser.new.parse_issue_numbers_from_pull_request_body(pull_request.body)
-      end
-
-      # Parse the pull request number from the HEROKU_APP_NAME.
-      # Alternatively, a PULL_REQUEST_NUMBER can explicitly be set.
-      def pull_request_number
-        return ENV["PULL_REQUEST_NUMBER"] if ENV["PULL_REQUEST_NUMBER"]
-        Planter.configuration.heroku_app_name.split("-pr-").last.to_i if Planter.configuration.heroku_app_name
-      end
-
-      # Fetch a pull request object from GitHub based on a repository
-      # name and pull request number.
-      def pull_request(repository_full_name, pr_number)
-        # Ensure that the pull request exists.
-        client = Octokit::Client.new(access_token: Planter.configuration.github_access_token)
-        client.pull_request(repository_full_name, pr_number)
-      rescue Octokit::NotFound
-        raise "Pull request ##{pr_number} for #{repository_full_name} was not found."
-      end
-
-      # Checks that the GitHub repository full name has been set and then returns it.
-      def repository_full_name
-        full_name = Planter.configuration.github_repository_full_name
-        unless full_name
-          raise "The GitHub repository full name has not been set."
-        end
-        full_name
       end
     end
   end
